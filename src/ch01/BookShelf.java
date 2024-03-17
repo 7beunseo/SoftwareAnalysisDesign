ec942b7512f4306e6fdd4f6efd1efe2260b33177
7beunseo <kimes0403@gmail.com>
2024-03-17 17:36:55 +0900

    week2 - BookShelf ArrayList 사용

--- src/ch01/BookShelf.java
+++ src/ch01/BookShelf.java
@@ -1,25 +1,27 @@
 package ch01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookShelf implements Iterable<Book>{
-    Book books[];
-    int index = 0;
+    List<Book> books;
 
     public BookShelf(int bookSize) {
-        books = new Book[bookSize];
+        books = new ArrayList<>(bookSize);
     }
 
     // 책꽂이에 꽂음
     public void insertBook(Book book) {
-        books[index++] = book;
+        books.add(book);
     }
 
     // 책 꺼내옴
     public Book getBook(int idx) {
-        return books[idx];
+        return books.get(idx);
     }
 
     public int getLength() {
-        return index;
+        return books.size();
     }
 
     @Override
