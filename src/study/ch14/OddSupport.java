6e86f67dd1b66506cc0eb0a0a4a36b634fcfde02
7beunseo <kimes0403@gmail.com>
2024-05-31 01:50:40 +0900

    week14 - ChainOfResponsibility

---
+++ src/study/ch14/OddSupport.java
@@ -0,0 +1,13 @@
+package study.ch14;
+
+public class OddSupport extends Support{
+    public OddSupport(String name) {
+        super(name);
+    }
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        if(trouble.getNumber() % 2 == 1)
+            return true;
+        return false;
+    }
+}
---
+++ src/study/ch14/Trouble.java
@@ -0,0 +1,15 @@
+package study.ch14;
+
+public class Trouble {
+    private int number;
+    public Trouble(int number) {
+        this.number = number;
+    }
+    public int getNumber() {
+        return number;
+    }
+    @Override
+    public String toString() {
+        return "[Trouble " + number + "]";
+    }
+}
